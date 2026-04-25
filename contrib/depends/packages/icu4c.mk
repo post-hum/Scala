f06e253bd7f1474d7891a672c3465b72ccfe1c7a
Posthuman <qw8245666@gmail.com>
2026-04-25 21:30:28 +0500

    Fix ICU depends build (configure + correct build dir + mingw support) FINNALLY

--- contrib/depends/packages/icu4c.mk
+++ contrib/depends/packages/icu4c.mk
@@ -5,51 +5,41 @@ $(package)_file_name=$(package)-55_2-src.tgz
 $(package)_sha256_hash=eda2aa9f9c787748a2e2d310590720ca8bcc6252adf6b4cfb03b65bef9d66759
 $(package)_patches=icu-001-dont-build-static-dynamic-twice.patch
 
-# --- FIX: новые config.sub / config.guess
 define $(package)_preprocess_cmds
-  cp -f $(BASEDIR)/config.guess $(BASEDIR)/config.sub source/ && \
-  patch -p1 < $($(package)_patch_dir)/icu-001-dont-build-static-dynamic-twice.patch
+	cp -f $(BASEDIR)/config.guess $(BASEDIR)/config.sub source/ && \
+	patch -p1 < $($(package)_patch_dir)/icu-001-dont-build-static-dynamic-twice.patch
 endef
 
-# --- flags (фикс GCC)
 define $(package)_set_vars
-  $(package)_build_opts= \
-    CFLAGS="$($(package)_cflags) $($(package)_cppflags) -DU_USING_ICU_NAMESPACE=0 -DU_STATIC_IMPLEMENTATION -DU_COMBINED_IMPLEMENTATION -fPIC" \
-    CXXFLAGS="$($(package)_cxxflags) -Wno-error=implicit-fallthrough -Wno-implicit-fallthrough"
+$(package)_build_opts= \
+	CFLAGS="$($(package)_cflags) $($(package)_cppflags) -DU_USING_ICU_NAMESPACE=0 -DU_STATIC_IMPLEMENTATION -DU_COMBINED_IMPLEMENTATION -fPIC" \
+	CXXFLAGS="$($(package)_cxxflags) -Wno-error=implicit-fallthrough -Wno-implicit-fallthrough"
 endef
 
-# --- configure (с cross-build)
 define $(package)_config_cmds
-  mkdir build-host && \
-  mkdir build-target && \
-
-  cd build-host && \
-  ../source/configure \
-    --disable-shared \
-    --enable-static && \
-  $(MAKE) && \
-
-  cd ../build-target && \
-  ../source/configure \
-    --host=$(host) \
-    --build=$(build) \
-    --prefix=$(host_prefix) \
-    --enable-static \
-    --disable-shared \
-    --disable-layout \
-    --disable-layoutex \
-    --disable-tests \
-    --disable-samples \
-    --with-data-packaging=static \
-    --with-cross-build=$(PWD)/build-host
+	mkdir -p build-host build-target && \
+	cd build-host && \
+	../source/configure --disable-shared --enable-static && \
+	$(MAKE) && \
+	cd ../build-target && \
+	../source/configure \
+		--host=$(host) \
+		--build=$(build) \
+		--prefix=$(host_prefix) \
+		--enable-static \
+		--disable-shared \
+		--disable-layout \
+		--disable-layoutex \
+		--disable-tests \
+		--disable-samples \
+		--with-data-packaging=static \
+		--with-cross-build=$(PWD)/build-host
 endef
 
-# --- build
 define $(package)_build_cmds
-  cd build-target && $(MAKE) $($(package)_build_opts)
+	cd build-target && $(MAKE) $($(package)_build_opts)
 endef
 
-# --- install
 define $(package)_stage_cmds
-  cd build-target && $(MAKE) DESTDIR=$($(package)_staging_dir) install
+	cd build-target && $(MAKE) DESTDIR=$($(package)_staging_dir) install
 endef
