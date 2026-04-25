cb83f579f04ccef6169e4edd0eba9718f3768a80
Posthuman <qw8245666@gmail.com>
2026-04-25 22:10:10 +0500

    Fix ICU depends build (configure + correct build dir + mingw support) UUUUUUHHHHHHH

--- contrib/depends/packages/icu4c.mk
+++ contrib/depends/packages/icu4c.mk
@@ -5,41 +5,35 @@ $(package)_file_name=$(package)-55_2-src.tgz
 $(package)_sha256_hash=eda2aa9f9c787748a2e2d310590720ca8bcc6252adf6b4cfb03b65bef9d66759
 $(package)_patches=icu-001-dont-build-static-dynamic-twice.patch
 
-define $(package)_preprocess_cmds
-	cp -f $(BASEDIR)/config.guess $(BASEDIR)/config.sub source/ && \
-	patch -p1 < $($(package)_patch_dir)/icu-001-dont-build-static-dynamic-twice.patch
-endef
-
 define $(package)_set_vars
-$(package)_build_opts= \
-	CFLAGS="$($(package)_cflags) $($(package)_cppflags) -DU_USING_ICU_NAMESPACE=0 -DU_STATIC_IMPLEMENTATION -DU_COMBINED_IMPLEMENTATION -fPIC" \
-	CXXFLAGS="$($(package)_cxxflags) -Wno-error=implicit-fallthrough -Wno-implicit-fallthrough"
+  $(package)_config_opts=
+  $(package)_config_opts += --disable-shared
+  $(package)_config_opts += --enable-static
 endef
 
 define $(package)_config_cmds
-	mkdir -p build-host build-target && \
-	cd build-host && \
-	../source/configure --disable-shared --enable-static && \
-	$(MAKE) && \
-	cd ../build-target && \
-	../source/configure \
-		--host=$(host) \
-		--build=$(build) \
-		--prefix=$(host_prefix) \
-		--enable-static \
-		--disable-shared \
-		--disable-layout \
-		--disable-layoutex \
-		--disable-tests \
-		--disable-samples \
-		--with-data-packaging=static \
-		--with-cross-build=$(PWD)/build-host
+  patch -p1 < $($(package)_patch_dir)/icu-001-dont-build-static-dynamic-twice.patch && \
+  cp config.guess config.sub source/ && \
+  mkdir -p build-host build-target && \
+
+  cd build-host && \
+  ../source/runConfigureICU Linux && \
+  $(MAKE) -j$(JOBS) && \
+
+  cd ../build-target && \
+  ../source/runConfigureICU Linux \
+    --prefix=$(host_prefix) \
+    --enable-static=yes \
+    --disable-shared \
+    --with-cross-build=$(CURDIR)/build-host
 endef
 
 define $(package)_build_cmds
-	cd build-target && $(MAKE) $($(package)_build_opts)
+  cd build-target && \
+  $(MAKE) -j$(JOBS)
 endef
 
 define $(package)_stage_cmds
-	cd build-target && $(MAKE) DESTDIR=$($(package)_staging_dir) install
+  cd build-target && \
+  $(MAKE) DESTDIR=$($(package)_staging_dir) install
 endef
